496d3e4127c5b9ca32ec958b2a07532e8084df15
Floakz <sergioamoreira@sapo.pt>
2026-01-17 15:23:50 +0000

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         const data = req.body;
 
         const result = await resend.emails.send({
-            from: 'noreply@syenadigital.com',
+            from: 'Syena@syenadigital.com',
             to: 'chikengorme@gmail.com',
             subject: 'Syena | Pedido de serviços',
             html: `
