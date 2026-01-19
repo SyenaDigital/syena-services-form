c9e89f6dddd7b8f89785b99120093e4553fa320b
Floakz <sergioamoreira@sapo.pt>
2026-01-19 15:11:51 +0000

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
         const result = await resend.emails.send({
             from: 'Syena@syenadigital.com',
             to: 'andre@syenadigital.com',
-            cc: ['sergio@syenadigital.com', 'bfm299@gmail.com'],
+            cc: ['sergio@syenadigital.com', 'propostas@syenadigital.com'],
             subject: 'Syena | Pedido de serviços',
             html: `
         <h2>Preenchimento de formulário de serviços</h2>
